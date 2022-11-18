bc4141c53d1e4f54d2bbc7c28fc6b3877ea6f447
Young Kim <ykim@iclinicemr.com>
2022-11-18 12:07:55 +0900

    Project initialized

---
+++ src/main/java/com/example/team0209/Team0209Application.java
@@ -0,0 +1,13 @@
+package com.example.team0209;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team0209Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team0209Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/team0209/Team0209ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.team0209;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team0209ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
